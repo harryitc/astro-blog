db891ba0e6d0ac34f30a9f60a3172a67e9e113f7
harryitc <cuongharryit@gmail.com>
2025-01-15 11:15:58 +0700

    Fix: remove base

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,5 @@ import preact from "@astrojs/preact";
 // https://astro.build/config
 export default defineConfig({
   site: "https://harryitc.github.io",
-  base: "/",
   integrations: [preact()]
 });
