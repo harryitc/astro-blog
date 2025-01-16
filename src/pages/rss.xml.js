11328d9aed931a09a8e0d1ee0cbe7918295e0cbc
harryitc <cuongharryit@gmail.com>
2025-01-17 00:27:50 +0700

    Fix: site url

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,10 +1,11 @@
 import rss, { pagesGlobToRssItems } from '@astrojs/rss';
+import { PROCESS_ENV } from '@configs/env';
 
 export async function GET(context) {
   return rss({
     title: 'Astro Learner | Blog',
     description: 'My journey learning Astro',
-    site: context.site,
+    site: PROCESS_ENV.SITE_URL  + '/rss.xml',
     items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
     customData: `<language>en-us</language>`,
   });
