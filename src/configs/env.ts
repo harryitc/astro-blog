31fcea3861f7e86e489a69dc28d2e3185e3c856b
harryitc <cuongharryit@gmail.com>
2025-01-17 00:50:11 +0700

    Feat: add validate

--- src/configs/env.ts
+++ src/configs/env.ts
@@ -1,11 +1,18 @@
 import dotenv from "dotenv";
-import { loadEnv } from "vite";
 
 // Note: all relative imports
 import { prettyPrintObject } from "../utils/log";
 
 
 const NODE_ENV = process.env.NODE_ENV;
+const nodeEnvValues = ["development", "production", "testing"];
+
+if (!nodeEnvValues.includes(NODE_ENV)) {
+    // eslint-disable-next-line no-console
+    console.error('Invalid process.env.NODE_ENV: ', NODE_ENV);
+    throw new Error('Invalid process.env.NODE_ENV');
+}
+
 const envFileName = `.env.${NODE_ENV}`;
 
 // Load environment variables from .env file and override existing ones
@@ -18,21 +25,7 @@ dotenv.config({ path: envFileName, override: true });
 // import.meta.env is not available in astro.config.mjs, only after the config is loaded.
 // ! MUST use process.env for vars used in astro.config.mjs.
 export const LogEnv = (): void => {
-    //   const NODE_ENV = process.env.NODE_ENV;
-    //   const envFileName = `.env.${NODE_ENV}`;
 
-    //   // Load environment variables from .env file and override existing ones
-    //   dotenv.config({ path: envFileName, override: true });
-
-    //   // console.log(loadEnv(process.env.SITE_URL, process.cwd(), ""));
-    //   console.log("[Starting Astro with config] Environment Variables:");
-    //   prettyPrintObject({
-    //     SITE_URL: process.env.SITE_URL,
-    //     BASE_PATH: process.env.BASE_PATH,
-    //     NODE_ENV: process.env.NODE_ENV,
-    //     BASE_URL: process.env.BASE_URL,
-    //     // more env variables...
-    //   });
 };
 
 export const PROCESS_ENV = {
