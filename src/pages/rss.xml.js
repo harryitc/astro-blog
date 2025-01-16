273cd2db6a36f651c81d168c1b3c37d744c87657
harryitc <cuongharryit@gmail.com>
2025-01-17 00:38:44 +0700

    Fix: path rss

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -5,7 +5,7 @@ export async function GET(context) {
   return rss({
     title: 'Astro Learner | Blog',
     description: 'My journey learning Astro',
-    site: PROCESS_ENV.SITE_URL  + '/rss.xml',
+    site: PROCESS_ENV.SITE_URL + PROCESS_ENV.BASE_PATH + '/rss.xml',
     items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
     customData: `<language>en-us</language>`,
   });
