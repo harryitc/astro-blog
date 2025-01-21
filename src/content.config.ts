1b2f888a2cbf2ce688d158b465d702e107a73a0d
harryitc <cuongharryit@gmail.com>
2025-01-22 01:02:25 +0700

    Fix: use default post to avoid undefined when build

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,6 +1,32 @@
 import { defineCollection, getCollection, z } from 'astro:content';
 import { glob } from 'astro/loaders';
 
+export interface PostCollections {
+  id: string;
+  layout: string;
+  title: string;
+  description: string;
+  image: {
+    url: string;
+    alt: string;
+  };
+  pubDate: string;
+  tags: string[];
+  author: string;
+}
+export interface PostSchema {
+  layout: string;
+  title: string;
+  description: string;
+  image: {
+    url: string;
+    alt: string;
+  };
+  pubDate: Date;
+  tags: string[];
+  author: string;
+}
+
 const posts = defineCollection({
   loader: glob({ pattern: "**/*.{md,mdx}", base: "./src/pages/posts" }),
   schema: z.object({
@@ -27,9 +53,25 @@ console.warn(`allBlogPostsFromCollections (${allBlogPostsFromCollections.length}
 //   console.log(`-> ${index}. ${post.data.title} (${post.filePath})`);
 // })
 
-export const AllPosts = () => allBlogPostsFromCollections.map((post, index) => {
+export const AllPosts = (): PostCollections[] => allBlogPostsFromCollections?.map((post, index) => {
   return {
     id: `posts/${post.id}`,
-    ...post.data
+    ...post.data,
+    pubDate: post.data.pubDate.toISOString().split('T')[0], // Format date to YYYY-MM-DD
   };
 });
+
+export const DEFAULT_POSTS: PostCollections =
+{
+  id: 'posts/NotFound',
+  layout: '../../layouts/PostLayout.astro',
+  title: 'NaN',
+  description: 'NaN',
+  pubDate: new Date().toISOString().split('T')[0], // Format date to YYYY-MM-DD
+  tags: [],
+  author: 'NaN',
+  image: {
+    url: 'https://via.placeholder.com/150',
+    alt: 'NaN'
+  }
+};
